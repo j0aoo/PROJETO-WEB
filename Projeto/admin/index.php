13268c7b00dd7337915313e8fc7daabc0a29595f
j0aoo <dk.joao12@gmail.com>
2019-02-20 14:48:39 -0300

    Tela principal admin / login

---
+++ Projeto/admin/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no" />
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+	<!-- apenas teste-->
+	<link href="https://fonts.googleapis.com/css?family=Lobster|Playfair+Display" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../fonts/themify/themify-icons.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../fonts/elegant-font/html-css/style.css">
+</head>
+<body>
+
+<div class="menu-cima">
+	<div class="geral-cima">
+		<h2>AdminLTE</h2>
+	</div>
+	<div class="ld-2">
+		<ul>
+			<li>
+				<img src="icone.png" class="img-ad">
+				<ul class="nav-drop">
+					<li>
+						<a href=""><h4>Sair</h4></a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+		<p>Name</p>
+	</div>
+</div>
+
+<input type="checkbox" id="check">
+<label id="icone" for="check"><img src="icone.png"></label>
+
+<div class="barra">
+	<nav class="nav-barra">
+		<a href="">
+			<div class="link">
+				<ul>
+					<li><h4 class="li-p">Carros Novos</h4>
+						<ul class="link-menu">
+							<li><a href="" class="item-link"><i class="fa fa-circle-o" aria-hidden="true"></i> Cadastrar</a></li>
+							<li><a href="" class="item-link"><i class="fa fa-circle-o" aria-hidden="true"></i> Alterar</a></li>
+							<li><a href="" class="item-link"><i class="fa fa-circle-o" aria-hidden="true"></i> Consultar / Excluir</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</a>
+		<a href="">
+			<div class="link">
+				<ul>
+					<li><h4 class="li-p">Carros Seminovos</h4>
+						<ul class="link-menu">
+							<li><a href="" class="item-link"><i class="fa fa-circle-o" aria-hidden="true"></i> Cadastrar</a></li>
+							<li><a href="" class="item-link"><i class="fa fa-circle-o" aria-hidden="true"></i> Alterar</a></li>
+							<li><a href="" class="item-link"><i class="fa fa-circle-o" aria-hidden="true"></i> Consultar / Excluir</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</a>
+	</nav>
+</div>
+
+
+</body>
+</html>
---
+++ Projeto/admin/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" type="text/css" href="css/styleLog.css">
+</head>
+<body>
+
+<div class="content">
+	<div class="geral-content">
+		<div class="form-gp">
+			<span>Login</span><br>
+			<input type="text" name="login" placeholder="Login" required class="form-jc"><br>	
+		</div>
+				
+		<div class="form-gp">
+			<span>Senha</span><br>
+			<input type="password" name="senha" placeholder="Senha" required class="form-jc"><br><br>
+		</div>	
+
+		<input type="submit" value="enviar" class="form-jc">
+	</div>
+</div>
+
+</body>
+</html>
